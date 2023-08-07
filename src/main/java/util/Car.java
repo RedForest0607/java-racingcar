f69671d5f95a60ce425fd422196a2e9d3dc16710
장인규(jjanga0607@pionnet.co.kr) <jjanga0607@pionnet.co.kr>
2023-08-07 13:24:22 +0900

    feat: 약간의 기능 추가
    - 여러대의 차를 한번에 등록하는 기능
    - 정적 팩토리 메소드를 통한 생성기능

--- src/main/java/util/Car.java
+++ src/main/java/util/Car.java
@@ -1,21 +1,57 @@
 package util;
 
+import static java.lang.Integer.*;
+
+import java.util.ArrayList;
+
 public class Car {
 	private String name;
+	private int speed;
 
 	public Car() {
 		name = "";
 	}
 
-	public Car(String name) {
-		if(name.length() < 1 || name.length() > 5) {
+	public Car(String name, int speed) {
+		if(name.length() < 1 || name.length() > 5 || speed < 0) {
 			throw new IllegalArgumentException();
 		}
 
 		this.name = name;
+		this.speed = speed;
 	}
 
 	public String[] split(String s) {
 		return s.split(",");
+	}
+
+	public static ArrayList<Car> createCar(String name, String speed) {
+
+		String[] names = name.split(",");
+		String[] speedList = speed.split(",");
+
+		if (names.length != speedList.length) {
+			throw new IllegalArgumentException();
+		}
+
+		ArrayList<Integer> integerSpeedList = new ArrayList<>();
+		for (int i = 0; i < speedList.length; i++) {
+			try {
+				integerSpeedList.add(Integer.parseInt(speedList[i]));
+			} catch (NumberFormatException e) {
+				throw new IllegalArgumentException();
+			}
+		}
+
+		ArrayList<Car> result = new ArrayList<>();
+
+		for (int i = 0; i < names.length; i++) {
+			try {
+				result.add(new Car(names[i], integerSpeedList.get(i)));
+			} catch (IllegalArgumentException e) {
+				throw new IllegalArgumentException();
+			}
+		}
+		return result;
 	}
 }
--- src/test/java/util/CarTest.java
+++ src/test/java/util/CarTest.java
@@ -1,5 +1,9 @@
 package util;
 
+import static util.Car.*;
+
+import java.util.ArrayList;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,14 +27,37 @@ public class CarTest {
 	@ValueSource(strings = {"박재성1234", "123456"})
 	@DisplayName("자동차 이름은 5자를 초과하면 실패한다.")
 	void 자동차_이름은_5자를_초과하면_실패(String name) {
-		Assertions.assertThatThrownBy(() -> new Car(name))
+		Assertions.assertThatThrownBy(() -> createCar(name, "5"))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@Test
 	@DisplayName("자동차 이름이 없을 경우 실패한다.")
 	void 자동차_이름은_최소_1글자_이상() {
-		Assertions.assertThatThrownBy(() -> new Car(""))
+		Assertions.assertThatThrownBy(() -> createCar("", "4"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("자동차의 속도가 음수인 경우 오류가 발생한다.")
+	void 자동차_속도는_자연수() {
+		// 다음과 같은 생성시의 이름과 속도에 대한 값들을 따로 변수로 빼 두는 것이 좋을까?
+		Assertions.assertThatThrownBy(() -> createCar("car", "-1"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("자동차를 생성시, 이름과 숫자가 여러개인 경우는 파싱해서 여러개를 생성해야 한다")
+	void 자동차_여러개_생성() {
+		ArrayList<Car> result = createCar("car1,car2,car3", "1,2,3");
+
+		Assertions.assertThat(result.size()).isEqualTo(3);
+	}
+
+	@Test
+	@DisplayName("자동차를 여러개 생성시, 입력값의 짝이 안맞는다면 오류가 발생한다")
+	void 자동차_여러개_생성시_짝이_안맞음() {
+		Assertions.assertThatThrownBy(() -> createCar("car1,car2,car3", "1,2"))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
